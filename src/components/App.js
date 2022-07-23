4370cae262e7075ac85c8923fcbce459f651ec7b
Win Feigle <win.feigle@gmail.com>
2022-07-23 17:15:17 -0600

    commit tree da4e6c986184ba7ef93cf7a02d68c28a768ac0fd

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,8 @@ import Table from "./Table";
 const API = "http://localhost:3001/sushis";
 
 function App() {
-const [sushis, setSushis] = useState([])
+const [sushis, setSushis] = useState([]);
+const [wallet, setWallet] = useState(100);
 
   useEffect(() => {
     fetch(API)
@@ -18,20 +19,26 @@ const [sushis, setSushis] = useState([])
       }); },[])
 
     const handleEatSushi = (eatenSushi) => {
+      if(wallet >= eatenSushi.price){
       const updatedSushis = sushis.map((sushi) => {
         if (sushi.id === eatenSushi.id) return { ...sushi, eaten: true };
         return sushi;
-    });
-
-    setSushis(updatedSushis);
+     });
+      setSushis(updatedSushis);
+      setWallet(prevWallet => prevWallet - eatenSushi.price)
+    } else{
+      alert(`You don't have enough money for ${eatenSushi.name}`);
+    }
   };
 
   const eatenSushis = sushis.filter(sushi => sushi.eaten)
 
+  
+
   return (
     <div className="app">
       <SushiContainer sushis={sushis} onEatSushi={handleEatSushi}/>
-      <Table plates={eatenSushis}/>
+      <Table plates={eatenSushis} wallet={wallet}/>
     </div>
   );
 }
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,15 +1,16 @@
 import React from "react";
 
-function Table({ plates = [] }) {
+function Table({ plates = [], wallet }) {
   // renders an empty plate for every element in the array
   const emptyPlates = plates.map((_, index) => (
     <div key={index} className="empty-plate" style={{ top: -7 * index }} />
   ));
 
+
   return (
     <>
       <h1 className="remaining">
-        You have: ${/* Give me how much money I have left */} remaining!
+        You have: ${wallet} remaining!
       </h1>
       <div className="table">
         <div className="stack">{emptyPlates}</div>
